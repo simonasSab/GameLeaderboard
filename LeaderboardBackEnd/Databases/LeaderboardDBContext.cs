54101c3eec270107c83dd82193c26531ac0ec2ea
Simonas Sabaliauskas <sim.sabaliauskas@gmail.com>
2024-06-14 19:19:48 +0300

    Forgot to add EF dbContext

---
+++ LeaderboardBackEnd/Databases/LeaderboardDBContext.cs
@@ -0,0 +1,17 @@
+﻿using LeaderboardBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaderboardBackEnd.Databases
+{
+    public class LeaderboardDBContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<Level> Levels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-OD4Q280;Database=GameLeaderboard;Integrated Security=True;TrustServerCertificate=true;");
+        }
+    }
+}
